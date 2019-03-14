0265863682a7cbe6506d63a6d563cf6e82e64340
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-03-14 12:12:35 +0900

    ダッシュボードページでページ一覧を取りに行くように修正

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="dashboard">
     ログイン後のページです。
+    <div v-for="page in pages" :key="page.title">
+      {{ page.title }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { mapActions, Actions } from '@/store/modules/pages';
+import { Getter } from 'vuex-class';
+import Page from '@/types/Page';
 
-@Component({})
-export default class DashBoard extends Vue {}
+@Component({
+  methods: {
+    ...mapActions(['getPagesAction'])
+  }
+})
+export default class DashBoard extends Vue {
+  @Getter('pages/pages') pages!: Page[];
+  getPagesAction!: () => void;
+
+  created() {
+    this.getPagesAction();
+  }
+}
 </script>
