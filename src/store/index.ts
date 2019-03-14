98536a8bef84adfc1055920633c7015b69ccd554
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-03-14 12:11:40 +0900

    pagesのstoreを作成

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { auth } from '@/store/modules/auth'
+import { pages } from '@/store/modules/pages'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    pages
   },
   state: {
   },
---
+++ src/store/modules/pages.ts
@@ -0,0 +1,69 @@
+import Vuex, { createNamespacedHelpers } from 'vuex';
+import { DefineActions, DefineGetters, DefineMutations } from 'vuex-type-helper';
+import { API } from 'aws-amplify';
+import Page from '@/types/Page';
+
+export interface State {
+  pages: Page[];
+}
+
+export interface Getters {
+  pages: Page[];
+}
+
+export interface Mutations {
+  getPages: {}
+}
+
+export interface Actions {
+  getPagesAction: {}
+}
+
+export const state: State = {
+  pages: []
+};
+
+export const getters: DefineGetters<Getters, State> = {
+  pages: (state) => state.pages,
+};
+
+export const mutations: DefineMutations<Mutations, State> = {
+  getPages(state) {
+    let apiName = 'api6ce98257';
+    let path = '/api'; 
+    let myInit = { // OPTIONAL
+        headers: {}, // OPTIONAL
+        response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+        queryStringParameters: {  // OPTIONAL
+            name: 'param'
+        }
+    }
+    API.get(apiName, path, myInit).then(response => {
+        // Add your code here
+        console.log(response)
+    }).catch(error => {
+        console.log(error.response)
+    });
+  },
+};
+
+export const actions: DefineActions<Actions, State, Mutations, Getters> = {
+  getPagesAction({ commit }) {
+    commit('getPages', {});
+  }
+};
+
+export const {
+  mapState,
+  mapGetters,
+  mapMutations,
+  mapActions,
+} = createNamespacedHelpers<State, Getters, Mutations, Actions>('pages');
+
+export const pages = {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
