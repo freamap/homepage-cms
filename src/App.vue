ae01c8cbc27f66213d3977c855a532d5908be2c5
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-03-14 12:11:03 +0900

    ダッシュボードを一時的に追加

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <div id="nav">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
+        <router-link to="/dashboard">ダッシュボード</router-link>
         <router-link to="/signin">ログイン</router-link>
         <router-link to="/signup">アカウント作成</router-link>
         <router-link to="/confirmCode">コード確認</router-link>
